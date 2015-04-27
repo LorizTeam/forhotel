1de57553b5450fcc8403535c404e5a8bff7febfe
lorizzimp <loriz.kiehiz@gmail.com>
2015-04-27 12:15:40 +0700

    create class for occupancy

--- src/com/smict/struts/data/booking3data.java
+++ src/com/smict/struts/data/booking3data.java
@@ -3,20 +3,25 @@ import java.util.*;
 import java.sql.*;
 
 public class booking3data {
-	public void booking_table(String roomId, String checkIn, String checkOut, String cusTitName, String cusNameSure, String cusEmail, String cusIntercode, String cusPhonenum, String cusCountry) throws ClassNotFoundException, SQLException{
+	public String booking_table(String roomId, String checkIn, String checkOut, String cusTitName, String cusNameSure, String cusEmail, String cusIntercode, String cusPhonenum, String cusCountry) throws ClassNotFoundException, SQLException{
 		dbconnect dbcon = new dbconnect();
 		Connection con = dbcon.DBconn_mysql();
 		String booking_id;
+		checkIn = "2015-05-01"; 
+		checkOut = "2015-05-06";
 		String sqlQuery = "call pro_booking ("+roomId+",'"+checkIn+" 00:00:00','"+checkOut+" 00:00:00','"+cusTitName+"','"+cusNameSure+"','"+cusEmail+"','"+cusIntercode+"','"+cusPhonenum+"','"+cusCountry+"');";
 		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery(sqlQuery);
 		rs.next();
-		booking_id = rs.getString("pbooking_id");
-		
+		return booking_id = rs.getString("pbooking_id");
 	}
 	
-	public void occupancy_table(){
-		
+	public void occupancy_table(String booking,String occupancy_title , String occupancy_name_sure){
+		dbconnect dbcon = new dbconnect();
+		Connection con = dbcon.DBconn_mysql();
+		String sqlQuery = "";
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(sqlQuery);
 	}
 	public void request_table(){
 		
